6517a0d92caaa50d2023e0576fe559b1c12c851a
dallen97 <101841823+dallen97@users.noreply.github.com>
2025-05-21 05:21:40 -0400

    created deletecontacts.php

---
+++ LAMPAPI/DeleteContacts.php
@@ -0,0 +1,52 @@
+<?php
+
+$inData = getRequestInfo();
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) 
+{
+    returnWithError($conn->connect_error);
+} 
+else
+{
+    $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ? AND UserID = ?");
+    $stmt->bind_param("ssi", $inData["firstName"],$inData["lastName"], $inData["userId"]);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0)
+    {
+        returnWithInfo("Contact deleted successfully");
+    }
+    else
+    {
+        returnWithError("Contact not found or permission denied");
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"results":[],"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($msg)
+{
+    $retValue = '{"results":[],"message":"' . $msg . '","error":""}';
+    sendResultInfoAsJson($retValue);
+}
+
+?>
