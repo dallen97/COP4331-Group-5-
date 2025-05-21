915c6a7445b650277562e56a0d20900df5b86f6b
dallen97 <101841823+dallen97@users.noreply.github.com>
2025-05-21 06:40:43 -0400

    Create UserSignUp.php

---
+++ LAMPAPI/UserSignUp.php
@@ -0,0 +1,64 @@
+<?php
+
+$inData = getRequestInfo();
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) 
+{
+    returnWithError($conn->connect_error);
+} 
+else
+{
+    // Check if username already exists
+    $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login = ?");
+    $stmt->bind_param("s", $inData["login"]);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows > 0)
+    {
+        returnWithError("Username already taken");
+    }
+    else
+    {
+        // Insert new user
+        $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $inData["firstName"], $inData["lastName"], $inData["login"], $inData["password"]);
+        if ($stmt->execute())
+        {
+            returnWithInfo("User registered successfully");
+        }
+        else
+        {
+            returnWithError("Failed to register user");
+        }
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"results":[],"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($msg)
+{
+    $retValue = '{"results":[],"message":"' . $msg . '","error":""}';
+    sendResultInfoAsJson($retValue);
+}
+
+?>
